298ef4ce1ca14c69ea66845969f1130698d3a1a3
dymnmysn <76868298+dymnmysn@users.noreply.github.com>
2024-11-19 17:22:30 +0300

    Update SalsaNext.py

--- train/tasks/semantic/modules/SalsaNext.py
+++ train/tasks/semantic/modules/SalsaNext.py
@@ -1,25 +1,29 @@
-# !/usr/bin/env python3
-# This file is covered by the LICENSE file in the root of this project.
-import imp
-
-import __init__ as booger
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+import torchvision.transforms as transforms
+import argparse
+import cv2
+import glob
+import os
+import numpy as np
+import sys
+from PIL import Image
 
 class ResContextBlock(nn.Module):
     def __init__(self, in_filters, out_filters):
         super(ResContextBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_filters, out_filters, kernel_size=(1, 1), stride=1)
-        self.act1 = nn.LeakyReLU()
+        self.act1 = nn.ReLU6()
 
         self.conv2 = nn.Conv2d(out_filters, out_filters, (3,3), padding=1)
-        self.act2 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm2d(out_filters)
+        self.act2 = nn.ReLU6()
+        #self.bn1 = nn.BatchNorm2d(out_filters)
 
         self.conv3 = nn.Conv2d(out_filters, out_filters, (3,3),dilation=2, padding=2)
-        self.act3 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm2d(out_filters)
+        self.act3 = nn.ReLU6()
+        #self.bn2 = nn.BatchNorm2d(out_filters)
 
 
     def forward(self, x):
@@ -28,12 +32,12 @@ class ResContextBlock(nn.Module):
         shortcut = self.act1(shortcut)
 
         resA = self.conv2(shortcut)
-        resA = self.act2(resA)
-        resA1 = self.bn1(resA)
+        #resA = self.bn1(resA)
+        resA1 = self.act2(resA)
 
         resA = self.conv3(resA1)
-        resA = self.act3(resA)
-        resA2 = self.bn2(resA)
+        #resA = self.bn2(resA)
+        resA2 = self.act3(resA)
 
         output = shortcut + resA2
         return output
@@ -46,23 +50,23 @@ class ResBlock(nn.Module):
         self.pooling = pooling
         self.drop_out = drop_out
         self.conv1 = nn.Conv2d(in_filters, out_filters, kernel_size=(1, 1), stride=stride)
-        self.act1 = nn.LeakyReLU()
+        self.act1 = nn.ReLU6()
 
         self.conv2 = nn.Conv2d(in_filters, out_filters, kernel_size=(3,3), padding=1)
-        self.act2 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm2d(out_filters)
+        self.act2 = nn.ReLU6()
+        #self.bn1 = nn.BatchNorm2d(out_filters)
 
         self.conv3 = nn.Conv2d(out_filters, out_filters, kernel_size=(3,3),dilation=2, padding=2)
-        self.act3 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm2d(out_filters)
+        self.act3 = nn.ReLU6()
+        #self.bn2 = nn.BatchNorm2d(out_filters)
 
         self.conv4 = nn.Conv2d(out_filters, out_filters, kernel_size=(2, 2), dilation=2, padding=1)
-        self.act4 = nn.LeakyReLU()
-        self.bn3 = nn.BatchNorm2d(out_filters)
+        self.act4 = nn.ReLU6()
+        #self.bn3 = nn.BatchNorm2d(out_filters)
 
         self.conv5 = nn.Conv2d(out_filters*3, out_filters, kernel_size=(1, 1))
-        self.act5 = nn.LeakyReLU()
-        self.bn4 = nn.BatchNorm2d(out_filters)
+        self.act5 = nn.ReLU6()
+        #self.bn4 = nn.BatchNorm2d(out_filters)
 
         if pooling:
             self.dropout = nn.Dropout2d(p=dropout_rate)
@@ -75,21 +79,21 @@ class ResBlock(nn.Module):
         shortcut = self.act1(shortcut)
 
         resA = self.conv2(x)
-        resA = self.act2(resA)
-        resA1 = self.bn1(resA)
+        #resA = self.bn1(resA)
+        resA1 = self.act2(resA)
 
         resA = self.conv3(resA1)
-        resA = self.act3(resA)
-        resA2 = self.bn2(resA)
+        #resA = self.bn2(resA)
+        resA2 = self.act3(resA)
 
         resA = self.conv4(resA2)
-        resA = self.act4(resA)
-        resA3 = self.bn3(resA)
+        #resA = self.bn3(resA)
+        resA3 = self.act4(resA)
 
         concat = torch.cat((resA1,resA2,resA3),dim=1)
         resA = self.conv5(concat)
+        #resA = self.bn4(resA)
         resA = self.act5(resA)
-        resA = self.bn4(resA)
         resA = shortcut + resA
 
 
@@ -121,26 +125,36 @@ class UpBlock(nn.Module):
         self.dropout2 = nn.Dropout2d(p=dropout_rate)
 
         self.conv1 = nn.Conv2d(in_filters//4 + 2*out_filters, out_filters, (3,3), padding=1)
-        self.act1 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm2d(out_filters)
+        self.act1 = nn.ReLU6()
+        #self.bn1 = nn.BatchNorm2d(out_filters)
 
         self.conv2 = nn.Conv2d(out_filters, out_filters, (3,3),dilation=2, padding=2)
-        self.act2 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm2d(out_filters)
+        self.act2 = nn.ReLU6()
+        #self.bn2 = nn.BatchNorm2d(out_filters)
 
         self.conv3 = nn.Conv2d(out_filters, out_filters, (2,2), dilation=2,padding=1)
-        self.act3 = nn.LeakyReLU()
-        self.bn3 = nn.BatchNorm2d(out_filters)
+        self.act3 = nn.ReLU6()
+        #self.bn3 = nn.BatchNorm2d(out_filters)
 
 
         self.conv4 = nn.Conv2d(out_filters*3,out_filters,kernel_size=(1,1))
-        self.act4 = nn.LeakyReLU()
-        self.bn4 = nn.BatchNorm2d(out_filters)
+        self.act4 = nn.ReLU6()
+        #self.bn4 = nn.BatchNorm2d(out_filters)
 
         self.dropout3 = nn.Dropout2d(p=dropout_rate)
 
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.convup = nn.Conv2d(in_filters, in_filters//4, kernel_size=(1,1))
+        #self.bnup = nn.BatchNorm2d(in_filters//4)
+        self.actup = nn.ReLU6()
+
     def forward(self, x, skip):
-        upA = nn.PixelShuffle(2)(x)
+        #upA = nn.PixelShuffle(2)(x)
+        upA = self.up(x)
+        upA = self.convup(upA)
+        #upA = self.bnup(upA)
+        upA = self.actup(upA)
+
         if self.drop_out:
             upA = self.dropout1(upA)
 
@@ -149,33 +163,33 @@ class UpBlock(nn.Module):
             upB = self.dropout2(upB)
 
         upE = self.conv1(upB)
-        upE = self.act1(upE)
-        upE1 = self.bn1(upE)
+        #upE = self.bn1(upE)
+        upE1 = self.act1(upE)
 
         upE = self.conv2(upE1)
-        upE = self.act2(upE)
-        upE2 = self.bn2(upE)
+        #upE = self.bn2(upE)
+        upE2 = self.act2(upE)
 
         upE = self.conv3(upE2)
-        upE = self.act3(upE)
-        upE3 = self.bn3(upE)
+        #upE = self.bn3(upE)
+        upE3 = self.act3(upE)
 
         concat = torch.cat((upE1,upE2,upE3),dim=1)
         upE = self.conv4(concat)
+        #upE = self.bn4(upE)
         upE = self.act4(upE)
-        upE = self.bn4(upE)
         if self.drop_out:
             upE = self.dropout3(upE)
 
         return upE
 
 
-class SalsaNext(nn.Module):
-    def __init__(self, nclasses):
-        super(SalsaNext, self).__init__()
+class Salsa(nn.Module):
+    def __init__(self, nclasses=20,inchannels=5):
+        super(Salsa, self).__init__()
         self.nclasses = nclasses
 
-        self.downCntx = ResContextBlock(5, 32)
+        self.downCntx = ResContextBlock(inchannels, 32)
         self.downCntx2 = ResContextBlock(32, 32)
         self.downCntx3 = ResContextBlock(32, 32)
 
@@ -209,6 +223,15 @@ class SalsaNext(nn.Module):
         up1e = self.upBlock4(up2e, down0b)
         logits = self.logits(up1e)
 
-        logits = logits
-        logits = F.softmax(logits, dim=1)
         return logits
+
+
+class SalsaNext(nn.Module):
+    def __init__(self, nclasses=20, inchannels=5):
+        super(SalsaNext, self).__init__()
+        self.salsa = Salsa(nclasses,inchannels)
+
+    def forward(self, x):
+        logits = self.salsa(x)
+        logits = F.softmax(logits, dim=1)
+        return logits(base)
